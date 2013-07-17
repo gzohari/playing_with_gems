aa2a4425e1b13fd82af9b2c2e775eb1cceb47c80
Gilad Zohari <gzohari@gmail.com>
2013-07-17 16:18:34 +0300

    added activeadmin and deleted some junk files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TestPhoto::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   resources :photos
 
